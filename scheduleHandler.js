572da994c2aa451ecbbbfd3a4ea9601a7d85f255
Parvizsho Aminov <paminov@cisco.com>
2018-11-27 18:58:37 -0800

    passing cognito sub to scheduling funcitons

--- scheduleHandler.js
+++ scheduleHandler.js
@@ -4,7 +4,9 @@ import * as schedulerLib from "./libs/scheduler-lib";
 //input: GET with queryparameter: ?startDate=<>,endDate=<>
 export async function getSchedule(event, context, callback) {
 	try {
-		const result = await schedulerLib.getSchedule(event.queryStringParameters.startDate, event.queryStringParameters.endDate);
+		const result = await schedulerLib.getSchedule(event.requestContext.identity.cognitoIdentityId,
+                                                      event.queryStringParameters.startDate,
+                                                      event.queryStringParameters.endDate);
 		if (result.Item) {
 			// Return the retrieved item
 			callback(null, success(result.Item));
@@ -22,7 +24,7 @@ export async function getSchedule(event, context, callback) {
 export async function reSchedule(event, context, callback) {
 	const data = JSON.parse(event.body);
         try {
-                await schedulerLib.reSchedule(data);
+                await schedulerLib.reSchedule(event.requestContext.identity.cognitoIdentityId, data);
                 callback(null, success({ status: true }));
         } catch (e) {
                 console.log(e);
